18e73c0c66b7f0082474238cdd27e5a4c727465c
Foyez-Ahamed <foyez1435@gmail.com>
2023-12-15 12:00:06 +0600

    small device navbar section updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -63,7 +63,7 @@ const Navbar = () => {
             </label>
             <ul
               tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 space-y-4] dark:text-black"
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 space-y-2 ] dark:bg-zinc-900 dark:text-white"
             >
               {navItems}
             </ul>
